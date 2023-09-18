b3a45b794bfe2b109dd4f222fe0ee7212ce4215a
timofey260 <smailovtimofey@gmail.com>
2023-09-18 22:52:17 +0300

    square

---
+++ src/AchievementHud.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HUD;
+using Menu.Remix.MixedUI;
+
+namespace AchievementTracker
+{
+	internal class AchievementHud : HudPart
+	{
+		public DyeableRect rect;
+		public AchievementHud(HUD.HUD hud) : base(hud)
+		{
+			rect = new(hud.fContainers[1], new(10, 10), new(200, 200));
+		}
+
+		public override void Draw(float timeStacker)
+		{
+			base.Draw(timeStacker);
+			rect.GrafUpdate(timeStacker);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			rect.Update();
+		}
+		public override void ClearSprites()
+		{
+			base.ClearSprites();
+			if (rect != null)
+			{
+				for (int i = 0; i < rect.sprites.Length; i++)
+				{
+					rect.sprites[i]?.RemoveFromContainer();
+				}
+			}
+			rect ??= null;
+		}
+	}
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,12 @@
 ﻿using BepInEx;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Security.Permissions;
-using Kittehface.Framework20;
+using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+//using Kittehface.Framework20;
 
 // Allows access to private members
 #pragma warning disable CS0618
@@ -12,15 +18,34 @@ namespace AchievementTracker;
 [BepInPlugin("timofey26.achievementtracker", "Achievement Tracker", "0.1.0")]
 sealed class Plugin : BaseUnityPlugin
 {
-	bool init;
+	public static List<RainWorld.AchievementID> achievements;
+	public static bool loadError;
 
 	public void OnEnable()
 	{
-		// Add hooks here
+		/*file folder: ((this.legacySaveFileIndex == 0) ? "sav" : ("sav_" + (this.legacySaveFileIndex + 1).ToString()));
+		 or just
+
+		Custom.LegacyRootFolderDirectory() + string.Concat(new string[]
+		{
+			Path.DirectorySeparatorChar.ToString(),
+			"UserData",
+			Path.DirectorySeparatorChar.ToString(),
+			text,
+			".txt"
+		}).ToLowerInvariant();
+		 
+		 */
 		On.RainWorld.OnModsInit += OnModsInit;
-		On.Player.Jump += Player_Jump;
-		On.Player.ThrownSpear += Player_ThrownSpear;
+		On.HUD.HUD.InitSinglePlayerHud += HUD_InitSinglePlayerHud;
+
 		On.RainWorld.AchievementAlreadyDisplayed += RainWorld_AchievementAlreadyDisplayed;
+	}
+
+	private void HUD_InitSinglePlayerHud(On.HUD.HUD.orig_InitSinglePlayerHud orig, HUD.HUD self, RoomCamera cam)
+	{
+		orig(self, cam);
+		self.AddPart(new AchievementHud(self));
 	}
 
 	private void Player_ThrownSpear(On.Player.orig_ThrownSpear orig, Player self, Spear spear)
@@ -42,12 +67,71 @@ sealed class Plugin : BaseUnityPlugin
 	private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
 	{
 		orig(self);
+		achievements = new();
+		string path = Custom.LegacyRootFolderDirectory() + string.Concat(new string[]
+		{
+			Path.DirectorySeparatorChar.ToString(),
+			"UserData",
+			Path.DirectorySeparatorChar.ToString(),
+			"ahievementsTracker.txt"
+		}).ToLowerInvariant();
+		try
+		{
+			if (File.Exists(path))
+			{
 
-		if (init) return;
+			}
+			else
+			{
+				File.Create(path);
+			}
+			string[] lines = File.ReadAllLines(path);
+			if (lines.Length > 0)
+			{
+				for (int i = 0; i < lines.Length; i++)
+				{
+					try
+					{
+						achievements.Add((RainWorld.AchievementID)Enum.Parse(typeof(RainWorld.AchievementID), lines[i].Trim()));
+					} catch (ArgumentException)
+					{
 
-		init = true;
+					}
+				}
+			}
 
+		} catch (Exception e)
+		{
+			switch (e) {
+				case UnauthorizedAccessException:
+					Debug.LogError("NO ACCESS TO FILES!!!");
+					break;
+				case DirectoryNotFoundException:
+					Debug.LogError("CANNOT FIND MOD DIRECTORY");
+					break;
+			}
+			loadError = true;
+		}
 		// Initialize assets, your mod config, and anything that uses RainWorld here
-		Logger.LogDebug("Hello world!");
+		if (!loadError)
+		{
+			Logger.LogDebug("Achivement tracker initialized!");
+		}
+	}
+	private void SaveAchievements()
+	{
+		string path = Custom.LegacyRootFolderDirectory() + string.Concat(new string[]
+		{
+			Path.DirectorySeparatorChar.ToString(),
+			"UserData",
+			Path.DirectorySeparatorChar.ToString(),
+			"ahievementsTracker.txt"
+		}).ToLowerInvariant();
+		string[] text = new string[achievements.Count];
+        for (int i = 0; i < achievements.Count; i++)
+        {
+			text[i] = achievements[i].ToString();
+        }
+        File.WriteAllLines(path, text);
 	}
 }
